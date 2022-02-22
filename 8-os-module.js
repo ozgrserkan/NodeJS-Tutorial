065b5ba7fe31729edd6230e94c10c2eab6c8a023
serkan <ozgr.serkan09@gmail.com>
2022-02-22 14:29:55 +0300

    path module and os module edited

--- 8-os-module.js
+++ 8-os-module.js
@@ -1,15 +1,11 @@
-const { log } = require("console");
 const os = require("os");
 
 // info about current user
-
 const user = os.userInfo();
-
 console.log(user);
 
 // method returns the system uptime in seconds
-
-console.log(`The System Uptime is ${os.uptime()} second`);
+console.log(`The System Uptime is ${os.uptime()} seconds`);
 
 const currentOS = {
   name: os.type(),
@@ -17,5 +13,4 @@ const currentOS = {
   totalMem: os.totalmem(),
   freeMem: os.freemem(),
 };
-
 console.log(currentOS);
