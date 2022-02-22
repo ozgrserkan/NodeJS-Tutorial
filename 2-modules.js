f25c54ba4b5188dcdcf1932fdbde3c24620f0492
ozgrserkan <ozgr.serkan09@gmail.com>
2022-02-22 11:34:54 +0300

    alternative syntax require and export

--- 2-modules.js
+++ 2-modules.js
@@ -1,7 +1,7 @@
-// modules
-
-const sayHi = (name) => {
-  console.log(`hello there ${name}`);
-};
-
-sayHi("serkan");
+const names = require('./04-names')
+const sayHi = require('./05-utils')
+const data = require('./06-alternative-flavor')
+require('./07-mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// modules
-
-const sayHi = (name) => {
-  console.log(`hello there ${name}`);
-};
-
-sayHi("serkan");
+const names = require("./3-names");
+const sayHi = require("./4-utils"); // you can use destructuring in here {john} = require("")
+const data = require("./5-alternative-flavor");
+sayHi("susan");
+sayHi(names.john);
+sayHi(names.peter);
+console.log(data);
